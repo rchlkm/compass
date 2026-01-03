5ef863a5626df824e876910adc8ac6fec786f1f6
Rachel Kim <rachelkim.tech@gmail.com>
2026-01-03 13:08:59 -0800

    Initial Commit

---
+++ Compass/CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassApp.swift
+//  Compass
+//
+//  Created by Rachel Kim on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
